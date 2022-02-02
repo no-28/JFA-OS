e81a6e8ecc539d49b99e1c0359bf8e2c3cf1c6d4
no-28 <snailvirus@outlook.com>
2022-02-02 17:19:27 +0800

    ed5

--- v3.x/v3.1/v3.1.asm
+++ v3.x/v3.1/v3.1.asm
@@ -4,13 +4,13 @@ MOV DS,AX
 MOV ES,AX
 CALL DSP
 JMP $
-MSG: DB "Hello, welcome to JFA-OS, a operating system that made by a middle school student who names Mu."
+MSG: DB "Hello, welcome to JFA-OS, an operating system that made by a middle school student who names Mu."
 DSP:
 MOV AX,MSG
 MOV BP,AX
 MOV AX,1301H
 MOV BL,000CH
-MOV CX,95
+MOV CX,96
 MOV DX,0
 INT 10H
 RET
